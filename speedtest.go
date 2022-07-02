a03efa2add763251e301a9d4a5183f7288922086
Rafał <rafdekar@gmail.com>
2022-07-02 19:06:54 +0200

    pkg change

--- speedtest.go
+++ speedtest.go
@@ -1,4 +1,4 @@
-package main
+package speedtest
 
 import (
 	"github.com/rafdekar/speedtest/api/provider"
@@ -10,6 +10,7 @@ const (
 	Netflix = "netflix"
 )
 
+// MeasureSpeed measures internet speed of user using specified provider. Provider can be either Netflix or OOkla.
 func MeasureSpeed(providerName string) (string, error) {
 	switch providerName {
 	case OOkla:
